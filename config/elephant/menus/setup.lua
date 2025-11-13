f3ace02bdfa3142ee6f2675d855dbe4b034021f7
3shaan <3shaan.dev@proton.me>
2025-11-13 14:23:24 +0600

    db and tui app add in elephant

--- config/elephant/menus/setup.lua
+++ config/elephant/menus/setup.lua
@@ -6,27 +6,34 @@ Icon = "󰉉"
 Parent = "menu"
 
 function GetEntries()
-    return {
-        {
-            Text = "Postgres",
-            Icon = "",
-            Actions = {
-                ["postgres"] = "walker --theme menus -m menus:postgres",
-            },
-        },
-        {
-            Text = "Docker",
-            Icon = "",
-            Actions = {
-                ["docker"] = "ghostty --class=local.floating -e docker-setup",
-            },
-        },
-        {
-            Text = "Node.js",
-            Icon = "",
-            Actions = {
-                ["nodejs"] = "ghostty --class=local.floating -e nodejs-setup",
-            },
-        },
-    }
+	return {
+		{
+			Text = "Postgres",
+			Icon = "",
+			Actions = {
+				["postgres"] = "walker --theme menus -m menus:postgres",
+			},
+		},
+		{
+			Text = "Docker",
+			Icon = "",
+			Actions = {
+				["docker"] = "ghostty --class=local.floating -e docker-setup",
+			},
+		},
+		{
+			Text = "Node.js",
+			Icon = "",
+			Actions = {
+				["nodejs"] = "ghostty --class=local.floating -e nodejs-setup",
+			},
+		},
+		{
+			Text = "Database in Docker",
+			Icon = "",
+			Actions = {
+				["docker-db"] = "ghostty --class=local.floating -e docker-db",
+			},
+		},
+	}
 end
